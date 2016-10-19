049b993416874219b9e4f9f1451db3df73be8ad2
D4rthB4n3 <dessel.bane@gmx.de>
2016-10-19 18:00:40 +0200

    Initial setup of async TreeView with async initlization

---
+++ WpfThreading/WpfThreading/DemoTreeViewItem.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfThreading
+{
+    public class DemoTreeViewItem : Base
+    {
+        #region Vars
+
+        private ObservableCollection<DemoTreeViewItem> _children = new ObservableCollection<DemoTreeViewItem>();
+        private bool _isExpanded;
+        private bool _isInitializing;
+        private int _level;
+        private string _name;
+        private bool _isSelected;
+        private bool _isInitialized;
+
+        #endregion
+
+        #region Properties
+
+        public string Name => _name;
+
+        public bool IsInitializing
+        {
+            get { return _isInitializing; }
+            private set
+            {
+                _isInitializing = value;
+                FirePropertyChanged();
+            }
+        }
+
+        public ObservableCollection<DemoTreeViewItem> Children => _children;
+
+        public bool IsExpanded
+        {
+            get { return _isExpanded; }
+            set
+            {
+                _isExpanded = value;
+
+                FirePropertyChanged();
+            }
+        }
+
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                _isSelected = value;
+                FirePropertyChanged();
+            }
+        }
+
+        public bool IsInitialized
+        {
+            get { return _isInitialized; }
+            private set
+            {
+                _isInitialized = value;
+                FirePropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public DemoTreeViewItem(string name, int level)
+        {
+            _name = name;
+            _level = level;
+            Thread.Sleep(300);
+        }
+
+        #endregion
+
+        public async void Initialize()
+        {
+            if (IsInitialized || IsInitializing)
+                return;
+
+            IsExpanded = true;
+            IsInitializing = true;
+            var childCol = await CreateChildren();
+
+            foreach (DemoTreeViewItem demoTreeViewItem in childCol)
+            {
+                _children.Add(demoTreeViewItem);
+                demoTreeViewItem.Initialize();
+            }
+
+            IsInitializing = false;
+            IsInitialized = true;
+        }
+
+
+        private Task<ICollection<DemoTreeViewItem>> CreateChildren()
+        {
+            return Task.Run(() =>
+            {
+                ICollection<DemoTreeViewItem> child = new Collection<DemoTreeViewItem>();
+                if (_level < 4)
+                    for (int i = 0; i < 2; i++)
+                    {
+                        child.Add(new DemoTreeViewItem($"{Name}.{i}", _level + 1));
+                    }
+                return child;
+            });
+        }
+    }
+}
---
+++ WpfThreading/WpfThreading/DemoViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfThreading
+{
+    public class DemoViewModel : Base
+    {
+        private ObservableCollection<DemoTreeViewItem> _items = new ObservableCollection<DemoTreeViewItem>();
+        private bool _isInitialized;
+        public ObservableCollection<DemoTreeViewItem> Items => _items;
+
+        public bool IsInitialized
+        {
+            get { return _isInitialized; }
+            private set
+            {
+                _isInitialized = value;
+                FirePropertyChanged();
+                FirePropertyChanged(nameof(CanInitialize));
+            }
+        }
+
+        public bool CanInitialize => !IsInitialized;
+
+        public DemoViewModel()
+        {
+            
+        }
+
+        public async void Initialize()
+        {
+            if(IsInitialized)
+                return;
+
+            IsInitialized = true;
+
+
+
+            var itm = await Task.Run(() => new DemoTreeViewItem($"Item {0}", 0));
+
+            _items.Add(itm);
+
+            itm.Initialize();
+        }
+    }
+}
