2c9d54225ed8791032285c07d6c7c68a1f5eddbf
DesselBane <dessel.bane@gmx.de>
2017-03-09 16:13:38 +0100

    Works (only for pdf anbd only with GhostScript installed)

---
+++ PrintTest/PrintTest/PrintManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Management;
+using System.Threading;
+using Ghostscript.NET.Processor;
+
+namespace PrintTest
+{
+    public static class PrintManager
+    {
+        public static void Print(string printDocPath, string printerName,bool showWindow = false)
+        {
+            using (var processor = new GhostscriptProcessor())
+            {
+                List<string> switches = new List<string>();
+                switches.Add("-empty");
+                switches.Add("-dPrinted");
+                switches.Add("-dBATCH");
+                switches.Add("-dNOPAUSE");
+                switches.Add("-dNOSAFER");
+                switches.Add("-dNumCopies=1");
+                switches.Add("-sDEVICE=mswinpr2");
+                switches.Add("-sOutputFile=%printer%" + printerName);
+                switches.Add("-f");
+                switches.Add(printDocPath);
+
+                processor.StartProcessing(switches.ToArray(), null);
+            }
+
+        }
+
+        public static void DetectPrinters()
+        {
+            var printerQuery = new ManagementObjectSearcher("SELECT * from Win32_Printer");
+            foreach (var printer in printerQuery.Get())
+            {
+                var name = printer.GetPropertyValue("Name");
+                var status = printer.GetPropertyValue("Status");
+                var isDefault = printer.GetPropertyValue("Default");
+                var isNetworkPrinter = printer.GetPropertyValue("Network");
+
+                Console.WriteLine("{0} (Status: {1}, Default: {2}, Network: {3}",
+                            name, status, isDefault, isNetworkPrinter);
+            }
+        }
+    }
+}
--- PrintTest/PrintTest/Program.cs
+++ PrintTest/PrintTest/Program.cs
@@ -10,6 +10,23 @@ namespace PrintTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Print Test v1.0");
+            do
+            {
+                Console.WriteLine("Please enter path to document or exit to quit.");
+                var path = Console.ReadLine();
+
+                if (path?.ToLower() == "exit")
+                    return;
+
+                PrintManager.DetectPrinters();
+
+                Console.WriteLine("Select a Printer");
+                var printerName = Console.ReadLine();
+
+                PrintManager.Print(path, printerName);
+
+            } while (true);
         }
     }
 }
